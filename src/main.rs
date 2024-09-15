2f2839a95a91a0bc7d7711e74e69a20f5f7cb0b2
Nahue <nlcostamagna@gmail.com>
2024-09-14 23:56:23 -0300

    initial project

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::net::TcpListener;
+
+// use rocket: https://rocket.rs/
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
+}
